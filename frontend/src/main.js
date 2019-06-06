071e0dc981d576f09ffc951b0f3522232a0ee3c1
Kobayashi Shunta <kobayashi@dptr.jp>
2019-06-06 18:49:34 +0900

    setup axios

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,12 +1,20 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
+import VueAxios from 'vue-axios'
+import { securedAxiosInstance, plainAxiosInstance } from './modules/axios'
 import './assets/stylesheet/main.css'
 
 Vue.config.productionTip = false
+Vue.use(VueAxios, {
+  secured: securedAxiosInstance,
+  plain: plainAxiosInstance
+})
 
 new Vue({
   el: '#app',
   router,
+  securedAxiosInstance,
+  plainAxiosInstance,
   render: h => h(App)
 })
