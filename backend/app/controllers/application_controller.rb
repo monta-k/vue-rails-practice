ec8b1e497511febabc514ed46f2d7ba262b0ba0c
Kobayashi Shunta <kobayashi@dptr.jp>
2019-06-12 13:44:59 +0900

    create login logout function

--- backend/app/controllers/application_controller.rb
+++ backend/app/controllers/application_controller.rb
@@ -1,6 +1,5 @@
 class ApplicationController < ActionController::API
   include ActionController::HttpAuthentication::Token::ControllerMethods
-  include SessionsHelper
   
   private
 
--- backend/app/controllers/signup_controller.rb
+++ backend/app/controllers/signup_controller.rb
@@ -1,12 +1,11 @@
 class SignupController < ApplicationController
   def create
     if decoded_token = authenticate_firebase_id_token
-      puts decoded_token
-      @user = User.create(
+      user = User.create(
         email: decoded_token['decode_token'][:payload]['email'],
         uid: decoded_token['uid']
       )
-      render json: @user, status: :created
+      render json: user, status: :created
     else
       render json: {message: "some error" }, status: :unprocessable_entity
     end
